ca8e4135035e701a8f929259013394bd793b728e
Emilia ☆ <emilia.mena26@gmail.com>
2019-09-07 14:00:20 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,30 +1,51 @@
 class Artist 
   attr_accessor :name
-  @@all=[]
+  
   
   def initialize(name)
     @name=name
+    
   end
   
-  def self.all
-    @@all
-  end 
-  
   def songs 
-    Song.all.select {|song|song.artist==self}
+    Song.all.select do |song|
+      song.artist == self
+    end 
   end
   
   def add_song(song)
-    song.artist = self
+    song.artist=self
   end
   
   def add_song_by_name(name)
-    song = Song.new(name)
-    song.artist = self
-  end 
-  
+    song=Song.new(name)
+    add_song(song)
+  end
+    
   def self.song_count
-    Song.all.count 
+    Song.all.size
   end
   
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- lib/author.rb
+++ lib/author.rb
@@ -1,30 +1,47 @@
-class Author
+class Author 
   attr_accessor :name
-  @@all=[]
   
   def initialize(name)
     @name=name
   end
   
-  def self.all
-    @@all
-  end 
-  
-  def posts
-    Post.all.select {|post|post.author==self}
+  def posts 
+    Post.all.select do |post|
+      post.author==self
+    end
   end
   
   def add_post(post)
-    post.author = self
+    post.author=self
   end
   
   def add_post_by_title(title)
-    post = Post.new(title)
-    post.author = self
-  end 
-  
-  def self.post_count
-    Post.all.count 
+    post=Post.new(title)
+    add_post(post)
   end
   
+  def self.post_count
+    Post.all.size
+  end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
